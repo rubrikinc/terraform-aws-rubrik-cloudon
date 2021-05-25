40bac96f43908959c5b7bd5021fa7faf8c4a3e27
Damani <damani@rubrik.com>
2021-05-25 14:31:07 -0700

    Create VMImport Role

--- variables.tf
+++ variables.tf
@@ -8,8 +8,9 @@ variable "iam_policy_name" {
   description = "The name of the IAM Policy configured with the correct CloudOn permissions."
   default     = "rubrik-cloud-on"
 }
-variable "archive_name" {
-  description = "The name of the Rubrik archive location in the Rubrik GUI."
+variable "iam_vmimport_policy_name" {
+  description = "The name of the IAM Policy configured with the correct permissions for the VM Import service."
+  default     = "rubrik-vmimport-role"
 }
 variable "vpc_id" {
   description = "The id of the vpc used to run bolt."
